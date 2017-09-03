c55ee73114976f54a972e1ab045fc9e8fa71c94c
zhanhgwei37 <zhanhgwei37@bj-m-207620a.local>
2017-09-03 17:37:22 +0800

    更新server文件显示console和port

--- 2/server.js
+++ 2/server.js
@@ -1,60 +1,62 @@
-var http = require('http');
-var fs  = require('fs');
-var path = require('path');
-var mime = require('mime');
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+var chatServer = require('./lib/chat_server');
 var cache = {};
 
-function send404(response) {
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: resource not found.');
-  response.end();
-}
-
-function sendFile(response, filePath, fileContents) {
-  response.writeHead(
-    200, 
-    {"content-type": mime.lookup(path.basename(filePath))}
-  );
-  response.end(fileContents);
-}
-
-function serveStatic(response, cache, absPath) {
-  if (cache[absPath]) {
-    sendFile(response, absPath, cache[absPath]);
-  } else {
-    fs.exists(absPath, function(exists) {
-      if (exists) {
-        fs.readFile(absPath, function(err, data) {
-          if (err) {
-            send404(response);
-          } else {
-            cache[absPath] = data;
-            sendFile(response, absPath, data);
-          }
-        });
-      } else {
-        send404(response);
-      }
+function send404(res) {
+    res.writeHead(404, {
+        'Content-Type' : 'text/plain'
     });
-  }
+    res.write('Error 404 : resource not found.');
+    res.end();
 }
 
-var server = http.createServer(function(request, response) {
-  var filePath = false;
+function sendFile(res, filePath, fileContents) {
+    res.writeHead(200, {
+        'content-type' : mime.lookup(path.basename(filePath))
+    });
+    res.end(fileContents);
+}
 
-  if (request.url == '/') {
-    filePath = 'public/index.html';
-  } else {
-    filePath = 'public' + request.url;
-  }
+function serveStatic(res, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(res, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if(exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if(err) {
+                        send404(res);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(res, absPath, data);
+                    }
+                });
+            } else {
+                send404(res);
+            }
+        });
+    }
+}
 
-  var absPath = './' + filePath;
-  serveStatic(response, cache, absPath);
+var server = http.createServer(function(req, res) {
+    console.log(req.headers.host, req.url);
+    var filePath = false;
+    
+    if(req.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + req.url;`find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'`
+    }
+
+    var absPath = './' + filePath;
+    serveStatic(res, cache, absPath);
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000.");
-});
-
-var chatServer = require('./lib/chat_server');
 chatServer.listen(server);
+
+server.listen(3003, function() {
+    console.log("Server listening on port 3000.");
+});
